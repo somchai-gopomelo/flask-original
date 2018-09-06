4d3dff34c2d62474937925b67eda8f07805ea794
somchai-gopomelo <31036795+somchai-gopomelo@users.noreply.github.com>
2018-09-06 19:27:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    html = "<h3>Hello, from GoPomelo !</h3>"
+    html = "<h3>Hello, GoPomelo !</h3>"
     return html
 
 if __name__ == "__main__":
