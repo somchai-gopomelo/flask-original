a05efcbd3aad206d8ccf9882d659535de09f6c5b
somchai-gopomelo <31036795+somchai-gopomelo@users.noreply.github.com>
2018-09-07 13:15:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ from flask import Flask, request, send_from_directory
 import os
 import socket
 
-app = Flask(__name__, static_url_path='')
+app = Flask(__name__, static_url_path='static')
 
-@app.route('/')
-def send_js(path):
+@app.route('/<path:path>')
+def send_page(path):
     return send_from_directory('static', path)
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=80)
