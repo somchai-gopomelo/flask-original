96e9e8a4955ef958eb5bb3b02ef50845dd113f68
somchai-gopomelo <31036795+somchai-gopomelo@users.noreply.github.com>
2018-09-07 14:43:46 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__, static_url_path='')
 
 @app.route('/<path:path>')
 def send_page(path):
-    return send_from_directory('static/index.html', path)
+    return send_from_directory('static', path)
+@app.route('/')
+def root():
+    return app.send_static_file('index.html')
+
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=80)
