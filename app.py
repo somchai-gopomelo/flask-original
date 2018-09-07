8c8b1f04d6192249c43391ff285fc0b909b662dc
somchai-gopomelo <31036795+somchai-gopomelo@users.noreply.github.com>
2018-09-07 11:04:18 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
-from flask import Flask
+from flask import Flask, request
 import os
 import socket
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 
 @app.route("/")
 def hello():
     html = "<h3>Hello, GoPomelo !</h3>"
     return html
+@app.route('/index/')
+def root():
+    return app.send_static_file('index.html')
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=80)
