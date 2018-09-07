c49225572c681f630636a42589f0fc1237038a58
somchai-gopomelo <31036795+somchai-gopomelo@users.noreply.github.com>
2018-09-07 13:13:07 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,12 +4,8 @@ import socket
 
 app = Flask(__name__, static_url_path='')
 
-@app.route("/")
-def hello():
-    html = "<h3>Hello, GoPomelo !</h3>"
-    return html
-@app.route('./mdl/')
+@app.route('/')
 def send_js(path):
-    return send_from_directory('mdl', path)
+    return send_from_directory('static', path)
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=80)
